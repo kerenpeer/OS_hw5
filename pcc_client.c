07a68d36351617b950c13ef76f4064f476eead8d
kerenpeer <kerenpeer97@gmail.com>
2021-12-23 14:53:35 +0200

    Start

---
+++ pcc_client.c
@@ -0,0 +1,118 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <errno.h> 
+ 
+ 
+ 
+ int main(int argc, char *argv[]){
+     unint32_t ip, N, C;
+     unint16_t port;
+     char *file_path, *ip_address, *N_transfer,*N_transfer_back, write_Buff[1024];
+     FILE *file;
+     int conv, N_bytes_Left, rc, sockfd = -1;
+     ssize_t read_b, write_b;
+     void *pointer;
+     int32_t nboC;
+ 
+
+     if(argc != 4){
+       perror("Wrong amount of parameters for client");
+       exit(1);  
+     }
+     ip_address = argv[1];
+     port = atoi(argv[2]);
+     file_path = argv[3];
+     
+     //Open the specified file for reading
+     file = fopen(file_path,"rb");
+     if(file == NULL){
+         perror("Failed to open file in client");
+         exit(1); 
+        }
+    //Create a TCP connection to the specified server port on the specified server IP.
+    struct sockaddr_in serv_addr; // where we Want to get to
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror("\n Error : Could not create socket \n");
+        exit(1);
+    }
+    if(conv = inet_pton(AF_INET,ip_address,ip) != 1){
+        if(conv == 0){
+            perror("\n Error : src does not contain a character string representing a valid network address in thespecified address family \n");
+        }
+        if(conv == -1){
+            perror("\n Error :  af does not contain a valid address family \n");
+        }
+        exit(1);
+    }
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    serv_addr.sin_addr.s_addr = ip; 
+    if(connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0){
+        perror("\n Error : Connect Failed. %s \n");
+        exit(1);
+    }
+
+    //Transfer the contents of the file to the server over the TCP connection
+    //compute N- the size of the file
+    fseek(file, 0L, SEEK_END);
+    N = (unint32_t)ftell(file);
+    fseek(file, 0L, SEEK_SET);
+    // N is in network byte order
+    N = htonl(N);
+
+    //The client sends the server N
+    N_transfer = (char*)&N;
+    N_bytes_Left = 0;
+    while(N_bytes_Left < N){
+        rc = write(sockfd, N_transfer+N_bytes_Left, N-N_bytes_Left);
+        if(rc < 0){
+            perror("\n Error : failed to write N in cilent");
+            exit(1);
+        }
+        N_bytes_Left += rc;
+    }
+    //Send the server N bytes - the file’s content
+    //First - read file content to buffer
+    read_b = fread(write_Buff,1,sizeof(write_Buff), file);
+    //finished reading file into buffer
+    if(read_b != sizeof(write_Buff)){
+        perror("\n Error : An error has occured or EOF in cilent");
+        exit(1); 
+    }
+    pointer = write_Buff;
+    while(read_b > 0){
+        write_b = write(sockfd, pointer, read_b);
+        if(write_b <=0){
+            perror("\n Error : problem in writing file to socket in cilent");
+            exit(1);
+        }
+        read_b -= write_b;
+        pointer += write_b;
+    }
+    //The server sends the client C, the number of printable characters
+    // read data from server into recv_buff block until there's something to read
+    N_transfer_back = (char*)&nboC; 
+    N_bytes_Left = 0;
+    while(N_bytes_Left < N){
+        read_b = read(sockfd, N_transfer+N_bytes_Left, N-N_bytes_Left);
+        if( read_b <= 0 ){
+            perror("\n Error : problem in reading C from socket in cilent");
+            exit(1);
+        }
+         N_bytes_Left += read_b;
+    }
+    fclose(file);
+    close(sockfd);
+    C = ntohl(nboC);
+    printf("# of printable characters: %u\n", C);
+    exit(0);
+ }
---
+++ pcc_server.c
@@ -0,0 +1,226 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <assert.h>
+#include <signal.h>
+
+int active_client;
+int kill_serv;
+//Initialize a data structure pcc_total - for ASCII char number i, pcc_total[i] will keep the number of observations from all clients.
+unint32_t pcc_total[127] = {0};
+
+/**
+ * @brief function for SIGINT handeling for srver
+ * 
+ */
+void SIGINT_handler(void){
+    int i;
+    if(active_client == 0){
+        kill_print();
+        exit(0);
+    }
+    else{
+        kill_serv = 1;
+    }
+}
+void kill_print(void){
+    int i;
+    for(i=32; i< 127; i++){
+        printf("char '%c' : %u times\n",i,pcc_total[i]);
+    }
+}
+
+int main(int argc, char *argv[]){
+    int listenfd  = -1, connfd = -1;
+    unit32_t N, nboN;
+    char *N_transfer;
+    int  N_bytes_Left = 0, read_b, i, vOfByte;
+    unint16_t port;
+
+    if(argc != 2){
+        perror("Wrong amount of parameters for client");
+        exit(1); 
+    }
+    port = atoi(argv[1]);
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in peer_addr;
+    socklen_t addrsize = sizeof(struct sockaddr_in);
+    char data_buff[1024];
+
+    
+    //init SIGINT handler
+    struct sigaction sig_handler;
+    sig_handler.sa_handler = &SIGINT_handler;
+    sigemptyset(&sig_handler.sa_mask);
+    sig_handler.sa_flags = SA_RESTART;
+    if(sigaction(SIGINT, sig_handler, 0) != 0){
+        perror("Error: failed to allocate SIGINT in sig_handler in server");
+        exit(1);
+    }
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0){
+        perror("Error: opening socket in server");
+        exit(1);
+    } 
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0){
+        perror("Error: setsockopt(SO_REUSEADDR) failed");
+        exit(1);
+
+    }
+    memset(&serv_addr, 0, addrsize);
+    serv_addr.sin_family = AF_INET;
+    // INADDR_ANY = any local machine address
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(port);
+    if( 0 != bind(listenfd, (struct sockaddr*) &serv_addr, addrsize)){
+        perror("\n Error : Bind Failed. %s \n");
+        exit(1);
+    }
+    //Listen to incoming TCP connections on the specified server port - queue of size 10.
+    if( 0 != listen(listenfd, 10)){
+        perror("\n Error : Listen Failed. %s \n");
+        exit(1);
+    }
+    /**
+     * @brief loop steps:
+     * 1) Accept TCP connection
+     * 2) Read a stream of bytes from the client
+     * 3) Computes the stream's printable character count
+     * 4) Write the result to the client over the TCP connection
+     * 5) Updates the pcc_total global data structure
+     */
+    N_transfer = (char*)&nboN;
+    while(kill_serv != 1){
+        /**
+         * @brief TODO: notice no skip_client_flag
+         */
+        connfd = accept(listenfd, (struct sockaddr*) &peer_addr, &addrsize);
+        if(connfd < 0){
+            perror("\n Error : Accept Failed. %s \n");
+            exit(1);
+        }
+        active_client = 1;
+        //read N from client
+        while(N_bytes_Left < sizeof(nboN)){
+            read_b = read(connfd, N_transfer, N_bytes_Left);
+            //if we recieved EOF while having more bytes to read
+            if(read_b ==0 & N_bytes_Left != 0){
+                close(connfd);
+                active_client = 0;
+                perror("\n Error : Connection terminated unexpectedly in server");
+                break;
+            }
+            else if(read_b == -1){
+                if(errno != ETIMEDOUT && errno != ECONNRESET && errno != EPIPE){
+                  perror("\n Error : read failed in server %s \n");
+                  exit(1);
+                }
+                else{
+                    close(connfd);
+                    active_client = 0;
+                    perror("\n Error : Connection terminated unexpectedly due to TCP errors in server");
+                    break;
+                }
+            }
+            //managed to read from client
+            else{
+                N_transfer += read_b;
+                N_bytes_Left += N_bytes_Left;
+
+            }
+        }
+        N = ntohl(nboN);
+        /**
+         * @brief Since The pcc_global statistics must not reflect the data received on the (terminated) connections - 
+         * we will initate a temporray pcc_temp array to hold the current statistics, 
+         * but will only add them to the global array iff the connection won't ne terminated.
+         */
+        unint32_t pcc_temp[127] = {0};
+        char data[1024];
+        N_bytes_Left = 0;
+        unit32_t C = 0;
+
+        read_b = read(connfd, data, sizeof(data));
+        N_bytes_Left += read_b;
+        if(read_b == -1){
+            if(errno != ETIMEDOUT && errno != ECONNRESET && errno != EPIPE){
+                perror("\n Error : read failed in server %s \n");
+                exit(1);
+            }
+            else{
+                close(connfd);
+                active_client = 0;
+                perror("\n Error : Connection terminated unexpectedly due to TCP errors in server");
+                break;
+            }
+        }
+        //count chars to temp_pcc
+        for(i=0; i < read_b; i++){
+            vOfByte = (int)data[i];
+            if(vOfByte>=32 && vOfByte<=126){
+                pcc_temp[vOfByte]++;
+                C++;
+            }
+        }
+        /**
+         * @brief if read_b == N, then the connection wasn't terminated 
+         * and we will write back C to cilent and add the statistics to pcc_total.
+         */
+        if(read_b == N){
+            unint32_t nboC = htonl(C);
+            N_transfer = (char*)&nboC;
+            N_bytes_Left = 0;
+            int rc;
+            while(N_bytes_Left < N){
+                rc = write(connfd, N_transfer, N-N_bytes_Left);
+                if(rc == 0 & N_bytes_Left < N){
+                    close(connfd);
+                    active_client = 0;
+                    perror("\n Error : Connection terminated unexpectedly in server");
+                    break;
+                }
+                else if(rc == -1){
+                    if(errno != ETIMEDOUT && errno != ECONNRESET && errno != EPIPE){
+                        perror("\n Error : read failed in server %s \n");
+                        exit(1);
+                    }
+                    else{
+                        close(connfd);
+                        active_client = 0;
+                        perror("\n Error : Connection terminated unexpectedly due to TCP errors in server");
+                        break;
+                    }
+                }
+                //managed to write to client
+                else{
+                    N_transfer += rc;
+                    N_bytes_Left += rc;
+
+                }
+            }
+            for(i=0; i< 127; i++){
+                pcc_total[i] += pcc_temp[i];
+            }
+        }
+        /**
+         * @brief if read_b != N, then the connection was terminated 
+         * and we won't write back C to cilent and won't add the statistics to pcc_total - 
+         * Just print an error message to stderr and keep accepting new client connections.
+         */
+        else{
+            perror("Client connection terminates due to TCP errors or unexpected connection close");
+        }
+        close(connfd);
+        active_client = 0;
+    }
+    kill_print();
+    exit(0);
+}
